d15877617500abcb0c540e1f38c82a59383abd7c
Henry <henrygraves95@gmail.com>
2019-09-19 11:33:01 -0700

    Adds LoginScreen.js boilerplate

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // styles
+});
+
+export default LoginScreen;
