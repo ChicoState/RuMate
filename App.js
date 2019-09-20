8cfdfd9eaebeb2073618ba8f24c320a0425b31a3
Henry <henrygraves95@gmail.com>
2019-09-19 22:35:25 -0700

    updates tab navigator replacing index with tasks screen

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import IndexScreen from './src/screens/IndexScreen';
 import HomeScreen from './src/screens/HomeScreen';
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
-  Index: IndexScreen,
+  Tasks: TasksScreen,
 });
 
 const App = createAppContainer(TabNavigator);
