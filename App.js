fdcb483bc025257fdb76569e63df05369a3a0c0b
Henry <henrygraves95@gmail.com>
2019-09-19 22:45:06 -0700

    Adds import for TaksScreen.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import HomeScreen from './src/screens/HomeScreen';
+import TasksScreen from './src/screens/TasksScreen';
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
