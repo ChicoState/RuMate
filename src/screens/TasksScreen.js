644a64c22a2b30458e8f29f9ea63024229b57cbd
Henry <henrygraves95@gmail.com>
2019-09-19 22:21:12 -0700

    Adds TasksScreen.js with boilerplate code

---
+++ src/screens/TasksScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TasksScreen = () => {
+  return (
+    <View>
+      <Text>Tasks</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // styles
+});
+
+export default TasksScreen;
