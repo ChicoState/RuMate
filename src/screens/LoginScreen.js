3bc0f056110245aa03fa12a6b8733f252c5d2ee0
Henry <henrygraves95@gmail.com>
2019-09-20 19:10:33 -0700

    adds text input fields for login creds / touchables for submitting data / adds style prop to relative elements

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  TouchableOpacity } from 'react-native';
+import Header from '../components/Header';
 
 const LoginScreen = () => {
+  // set initial email state to empty string
+  const [email, setEmail] = useState(""); 
+  // password string initally empty
+  const [password, setPassword] = useState("");
   return (
     <View>
-      <Text>Login</Text>
+      <Header text="Login to RuMate"/>
+      {/* Text input for user's login email */}
+      <TextInput style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+      {/* Text input for user's login pw */}
+      <TextInput style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+      {/* custom login button 
+        * (don't use <Button /> unless 
+        sepcifically wanted) */}
+      <TouchableOpacity onPress={ /* function to login */ }>
+        <View style={styles.submit}>
+          <Text>Submit</Text>
+        </View>
+      </TouchableOpacity>
+
+      {/* Login w/ google option */}
+      <TouchableOpacity>
+       {/* <GoogleAuth /> */}
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   // styles
+  input: {
+
+  },
+  submit: {
+
+  }
 });
 
 export default LoginScreen;
