6d714107566b53959dbdbf28ed0427da17abf165
Henry <henrygraves95@gmail.com>
2019-09-23 18:01:43 -0700

    Adds styling, navigation functionality to main app view

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,41 +1,46 @@
 import React, { useState } from 'react';
 import { 
   View, 
-  Text, 
+  Text,
   StyleSheet, 
-  TextInput, 
+  TextInput,
   TouchableOpacity } from 'react-native';
 import Header from '../components/Header';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}) => {
   // set initial email state to empty string
-  const [email, setEmail] = useState(""); 
+  const [email, setEmail] = useState("");
   // password string initally empty
   const [password, setPassword] = useState("");
   return (
     <View>
-      <Header text="Login to RuMate"/>
+      <Header style={styles.header} title="Login to RuMate"/>
       {/* Text input for user's login email */}
       <TextInput style={styles.input}
         value={email}
         onChangeText={setEmail}
+        placeholder="e-mail"
       />
       {/* Text input for user's login pw */}
       <TextInput style={styles.input}
         value={password}
         onChangeText={setPassword}
+        placeholder="password"
       />
       {/* custom login button 
         * (don't use <Button /> unless 
         sepcifically wanted) */}
-      <TouchableOpacity onPress={ /* function to login */ }>
+      <TouchableOpacity onPress={() => {
+        navigation.navigate('Main');
+      }}>
         <View style={styles.submit}>
           <Text>Submit</Text>
         </View>
       </TouchableOpacity>
 
       {/* Login w/ google option */}
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.submit}>
+        <Text>Login w/ Google</Text>
        {/* <GoogleAuth /> */}
       </TouchableOpacity>
     </View>
@@ -45,10 +50,16 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   // styles
   input: {
-
+    borderWidth: 1,
+    marginVertical: 2,
+    width: 300,
+    marginHorizontal: 5,
+    alignSelf: 'center'
   },
   submit: {
-
+    marginHorizontal: 5,
+    alignSelf: 'center',
+    marginVertical: 5
   }
 });
 
