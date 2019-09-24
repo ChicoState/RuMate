0068470361fe11b69acb319d51eb211d129ac96e
Henry <henrygraves95@gmail.com>
2019-09-23 18:00:13 -0700

    Adds multiple navigator functionality

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { useScreens } from 'react-native-screens';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import TasksScreen from './src/screens/TasksScreen';
+import LoginScreen from './src/screens/LoginScreen';
+useScreens();
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
   Tasks: TasksScreen,
+},{
+  initialRouteName: 'Home',
 });
 
-const App = createAppContainer(TabNavigator);
+const StackNavigator = createStackNavigator({
+  Login: LoginScreen,
+  Main: TabNavigator
+},{
+  initialRouteName: 'Login',
+  defaultNavigationOptions: {
+    // title: 'RuMate'
+    headerLeft: null,
+    gesturesEnabled: false
+  }
+});
+
+const App = createAppContainer(StackNavigator);
 
 export default () => {
   return (
