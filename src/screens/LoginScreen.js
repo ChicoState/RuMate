6505812a435649ee4bdd124671eea89c251f6557
Henry <henrygraves95@gmail.com>
2019-10-02 14:46:23 -0700

    Add conditional login via state

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -21,48 +21,69 @@ const LoginScreen = ({navigation}) => {
     // mongodb init goes here eventually.
   }, []);
 
+  const validate = (password, email) => {
+    console.log(password);
+    if (password && email)
+      return true;
+    return false;
+  }
 
-  return (
-    <View>
-      <Header style={styles.header} title="Login to RuMate"/>
-      {/* Text input for user's login email */}
-      <TextInput style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        placeholder="e-mail"
-      />
-      {/* Text input for user's login pw */}
-      <TextInput style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="password"
-      />
-      {/* custom login button 
-        * (don't use <Button /> unless 
-        sepcifically wanted) */}
-      <TouchableOpacity onPress={() => {
-        // IFF authenticate -> navigate
-        navigation.navigate('Main');
-      }}>
-        <View style={styles.submit}>
-          <Text>Submit</Text>
+  const isSignedIn = () => {
+    if (!signedIn) {
+      return (
+        <View>
+          <Header style={styles.header} title="Login to RuMate"/>
+          {/* Text input for user's login email */}
+          <TextInput style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="e-mail"
+          />
+          {/* Text input for user's login pw */}
+          <TextInput style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="password"
+          />
+          {/* custom login button 
+            * (don't use <Button /> unless 
+            sepcifically wanted) */}
+          <TouchableOpacity onPress={() => {
+            // IFF authenticate -> navigate
+            // alert("Non functional")
+            // if (validate(password, email))
+            //   navigation.navigate('Main');
+          }}>
+            <View style={styles.submit}>
+              <Text>Submit</Text>
+            </View>
+          </TouchableOpacity>
+    
+          {/* Login w/ google option */}
+          <TouchableOpacity style={styles.submit}>
+            <Text>Login w/ Google</Text>
+           {/* <GoogleAuth /> */}
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.submit}>
+            <Text>Login w/ Facebook</Text>
+           {/* <GoogleAuth /> */}
+          </TouchableOpacity>
+    
+    
         </View>
-      </TouchableOpacity>
+      );
+    } else {
+      navigation.navigate('Main');
+    }
+  }
 
-      {/* Login w/ google option */}
-      <TouchableOpacity style={styles.submit}>
-        <Text>Login w/ Google</Text>
-       {/* <GoogleAuth /> */}
-      </TouchableOpacity>
-      
-      <TouchableOpacity style={styles.submit}>
-        <Text>Login w/ Facebook</Text>
-       {/* <GoogleAuth /> */}
-      </TouchableOpacity>
-
-
+  return(
+    <View>
+      {isSignedIn()}
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
