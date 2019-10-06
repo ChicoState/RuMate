d861e0c5217461273b0b1cdc246341e870ff4595
David Luther <dluther@goldensoftware.com>
2019-10-06 00:35:16 -0700

    mark task as complete && tasks only created if input fields all full

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,6 @@
 import React,  {Component} from 'react';
-import { View, TouchableOpacity, FlatList, StyleSheet, Text,} from 'react-native';
+import { View, FlatList, StyleSheet, Text, TouchableHighlight,} from 'react-native'; 
+import Dialog from 'react-native-dialog';
 var firebase = require("firebase");
 
 class FlatListItem extends Component {
@@ -65,6 +66,8 @@ export default class TaskList extends Component {
         super(props);
         this.state = {
           flatlistData: [],
+          dialogVisible: false,
+          lastPressedItem: undefined
         }
     }
 
@@ -78,11 +81,16 @@ export default class TaskList extends Component {
       let newState = [];
 
       for(let item in tasks){
-        newState.push({
-          name: tasks[item].name,
-          description: tasks[item].description,
-          date: tasks[item].date,
-        });
+        if (tasks[item].completed == false)
+        {
+          newState.push({
+            name: tasks[item].name,
+            description: tasks[item].description,
+            date: tasks[item].date,
+            completed: tasks[item].completed,
+            key: item
+          });
+        }
       }
 
       this.setState({
@@ -91,6 +99,29 @@ export default class TaskList extends Component {
 
   });
 }
+
+  markAsCompleted = () => {
+    this.setState({
+      dialogVisible: false
+    });
+
+    firebase.database().ref('tasks/' + this.state.lastPressedItem.key).update({
+      completed: true
+    });
+  }
+
+  openDialog(item) {
+    this.setState({
+      lastPressedItem: item,
+      dialogVisible: true
+    });
+  }
+
+  cancel = () => {
+    this.setState({
+      dialogVisible: false
+    });
+  }
 
 
 render() {
@@ -101,14 +132,36 @@ render() {
         getItemLayout={(data, index) => (
           {length: 20, offset: 20 * index, index}
           )}
-        renderItem={({ item, index })=>{
+        renderItem={({ item, index, separators })=>{
           return (
-            <FlatListItem
-            item={item}
-            index={index}>
-            </FlatListItem>);
+            <TouchableHighlight
+            onPress={() => this.openDialog(item) }
+            onShowUnderlay={separators.highlight}>
+              <FlatListItem
+              item={item}
+              index={index}>
+              </FlatListItem>
+            </TouchableHighlight>);
         }}
       />
+      <Dialog.Container
+      visible={this.state.dialogVisible}
+      >
+        <Dialog.Title>Complete a Task</Dialog.Title>
+        <Dialog.Description>
+          Do you want to mark this task as completed?
+        </Dialog.Description>
+        <Dialog.Button
+        label='No'
+        onPress={this.cancel}
+        >
+        </Dialog.Button>
+        <Dialog.Button
+        label='Yes'
+        onPress={this.markAsCompleted}
+        >        
+        </Dialog.Button>
+      </Dialog.Container>
     </View>
   );
 }
