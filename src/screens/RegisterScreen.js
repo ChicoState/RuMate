4e9b749e1b0910d255102b71a6bfe8b7dbba85f5
Henry <henrygraves95@gmail.com>
2019-10-06 16:55:21 -0700

    add registration screen

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <Text> Register </Text>
+    </View>
+  );
+}
