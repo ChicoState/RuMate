3c860ed109bc49b26afeefc8f5a799936208db90
Henry <henrygraves95@gmail.com>
2019-10-07 13:20:05 -0700

    add bill list component

---
+++ src/components/BillList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Bill from '../components/Bill';
+
+const BillList = () => {
+  return (
+    <View>
+      <Bill />
+    </View>
+  );
+}
+
+styles = StyleSheet.create({
+
+});
+
+export default BillList;
