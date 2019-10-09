ca7993372f1c9cfbc98349a7e97f18210f58824f
Henry <henrygraves95@gmail.com>
2019-10-09 15:53:59 -0700

    working on navigation params passing

--- src/screens/BillsScreen.js
+++ src/screens/BillsScreen.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import BillList from '../components/BillList';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Header from '../components/Header';
+
+const renderAddBill = () => {
+  return (
+    <View>
+      <TouchableOpacity 
+        onPress={() => {
+          // navigation.navigate('CreateBill')
+        }}
+      >
+        <Text>Add Bill</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const BillsScreen = () => {
   const [user, setUser] = useState("");
   return (
     <View>
-      <Text> My Bills:  </Text>
+      <Header title="Bills" />
+      <Text> My Bills: </Text>
+      {renderAddBill()}
       <BillList user={() => {}} />
     </View>
   );
