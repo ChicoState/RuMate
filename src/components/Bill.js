db6f18be09129cd7c6414fc74910d347422b6fd1
Henry <henrygraves95@gmail.com>
2019-10-09 15:51:30 -0700

    add bill numbers

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const Bill = () => {
+  const [billNumber, setBillNumber] = useState(0);
+
   return (
     <View>
-      <Text> Bill </Text>
+      <Text> Bill { billNumber }</Text>
     </View>
   );
 }
