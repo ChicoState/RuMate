aa251a23ae918cf2a5caf4d72df6d750417626d7
Henry <henrygraves95@gmail.com>
2019-10-09 15:54:14 -0700

    add create bill screen

---
+++ src/screens/CreateBillScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CreateBillScreen = () => {
+  return (
+    <View>
+      <Text>CreateBillScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default CreateBillScreen;
