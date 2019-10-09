114cd533b662777f5b5e6fddc92ace440488e18f
Henry <henrygraves95@gmail.com>
2019-10-09 15:52:33 -0700

    started personalization features

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Header title="Home" />
+      <Text>Welcome {}</Text>
+      {console.log(navigation.state.params)}
     </View>
   );
 }
