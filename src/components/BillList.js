cdbd65ab4bb9b9330062344abd68ebe967616817
Henry <henrygraves95@gmail.com>
2019-10-10 12:12:21 -0700

    add flatlist

--- src/components/BillList.js
+++ src/components/BillList.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 
-const renderBills = () => {
+const renderBills = (bills, setBills) => {
   // firebase pull
+  <FlatList
+    keyExtractor={() => {
+
+    }}
+    data={bills}
+    renderItem={({item}) => {
+      return (
+        <View>
+          <Text>Test</Text>
+          {item.value}
+        </View>
+      );
+    }}
+  />
 }
 
 const BillList = () => {
+  const [bills, setBills] = useState([{name: "bill", value:"100"}]);
   return (
     <View>
-      {renderBills()}
+      {renderBills(bills, setBills)}
     </View>
   );
 }
