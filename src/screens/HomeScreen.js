6fcf7e1ec2dd25f68aaecc4bc70ef7fa16d097f2
Henry <henrygraves95@gmail.com>
2019-10-10 11:17:10 -0700

    add UID output

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Header from '../components/Header';
+import firebase from 'firebase';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Header title="Home" />
-      <Text>Welcome {}</Text>
+      <Text>Welcome {firebase.auth().currentUser.uid}</Text>
     </View>
   );
 }
