619738faaed33ce48de385f000b0a986f525cbe5
Henry <henrygraves95@gmail.com>
2019-10-13 15:25:51 -0700

    add bills data pull for current uid

--- src/components/BillList.js
+++ src/components/BillList.js
@@ -1,28 +1,52 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import firebase from 'firebase'
+import Bill from '../components/Bill';
 
+const getBills = async (setBills) => {
+  const bills = await firebase.database().ref('bills/');
+  bills.on('value', (snapshot) => {
+    const data = snapshot.val();
+    let list = [];
+    for (let item in data) {
+      if (firebase.auth().currentUser.uid == data[item].uid) {
+        list.push(data[item])
+      }
+    }
+    setBills(list);
+  })
+}
+
+const renderBill = (bills, item) => {
+  for (let bill in bills) {
+    <Text>
+      <Bill 
+        name = {bills[bill].name} 
+        value = {bills[bill].value}  
+      />
+    </Text>
+  }
+}
 
 const renderBills = (bills, setBills) => {
   // firebase pull
+  useEffect(() => {
+    getBills(setBills);
+  }, []);
+  console.log(bills);
   <FlatList
-    keyExtractor={() => {
-
-    }}
+    keyExtractor={item => item.uid}
     data={bills}
     renderItem={({item}) => {
       return (
-        <View>
-          <Text>Test</Text>
-          {item.value}
-        </View>
+        renderBill(bills, item)
       );
     }}
   />
 }
 
 const BillList = () => {
-  const [bills, setBills] = useState([{name: "bill", value:"100"}]);
+  const [bills, setBills] = useState([]);
   return (
     <View>
       {renderBills(bills, setBills)}
