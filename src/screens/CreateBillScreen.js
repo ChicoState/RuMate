8bfe73bd572eae4b349acb7b2bf6032359f6a70c
Henry <henrygraves95@gmail.com>
2019-10-13 16:33:04 -0700

    added uniq identifiers / due by fields

--- src/screens/CreateBillScreen.js
+++ src/screens/CreateBillScreen.js
@@ -6,6 +6,7 @@ import firebase from 'firebase';
 const CreateBillScreen = () => {
   const [name, setName] = useState("");
   const [value, setValue] = useState("");
+  const [date, setDate] = useState("");
   return (
     <View>
       <Header title="Add a bill"/>
@@ -19,13 +20,20 @@ const CreateBillScreen = () => {
         value={value}
         onChangeText={setValue}
       />
+      <Text>Due by</Text>
+      <TextInput style={styles.input}
+        value={date}
+        onChangeText={setDate}
+      />
       <Button title="Submit"
         onPress={() => {
           let bill = firebase.database().ref().child('/bills').push();
           bill.set({
-            name: name,
-            value: value,
+            name,
+            value,
+            date,
             uid: firebase.auth().currentUser.uid,
+            billId: firebase.auth().currentUser.uid + value + name + date
           });
         }}
       />
