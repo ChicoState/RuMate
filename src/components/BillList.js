09e30279a3794f60b1e3bc0be111f55b561adad7
Henry <henrygraves95@gmail.com>
2019-10-13 16:34:38 -0700

    add bill rendering from database

--- src/components/BillList.js
+++ src/components/BillList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import firebase from 'firebase'
 import Bill from '../components/Bill';
 
@@ -10,6 +10,7 @@ const getBills = async (setBills) => {
     let list = [];
     for (let item in data) {
       if (firebase.auth().currentUser.uid == data[item].uid) {
+        console.log("found one")
         list.push(data[item])
       }
     }
@@ -17,39 +18,36 @@ const getBills = async (setBills) => {
   })
 }
 
-const renderBill = (bills, item) => {
-  for (let bill in bills) {
-    <Text>
-      <Bill 
-        name = {bills[bill].name} 
-        value = {bills[bill].value}  
-      />
-    </Text>
-  }
-}
-
-const renderBills = (bills, setBills) => {
-  // firebase pull
-  useEffect(() => {
-    getBills(setBills);
-  }, []);
+const renderBills = (bills) => {
   console.log(bills);
-  <FlatList
-    keyExtractor={item => item.uid}
-    data={bills}
-    renderItem={({item}) => {
-      return (
-        renderBill(bills, item)
-      );
-    }}
-  />
+  return (
+    <FlatList 
+      keyExtractor={item => item.billId}
+      data={bills}
+      renderItem={({item}) => {
+        return (
+          <View>
+            <Bill 
+              name = {item.name}
+              value = {item.value}
+              due = {item.date}
+            />
+          </View>
+        );
+      }}
+    />
+  );
 }
 
 const BillList = () => {
   const [bills, setBills] = useState([]);
+  useEffect(() => {
+    // firebase pull
+    getBills(setBills);
+  }, []);
   return (
     <View>
-      {renderBills(bills, setBills)}
+      {renderBills(bills)}
     </View>
   );
 }
@@ -57,7 +55,7 @@ const BillList = () => {
 styles = StyleSheet.create({
   plusButton: {
     fontSize: 20
-  }
+  },
 });
 
 export default BillList;
