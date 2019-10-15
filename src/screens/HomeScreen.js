1abfda9f77ca9e54d546d0be139c7fd94dcf927a
Henry <henrygraves95@gmail.com>
2019-10-14 22:16:09 -0700

    replaced homebaked header with react native elements header to keep consistant

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Header from '../components/Header';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { Header } from 'react-native-elements';
 import firebase from 'firebase';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Header title="Home" />
+      <StatusBar barStyle='light-content'/>
+      <Header
+        backgroundColor="#119"
+        centerComponent={{text: "Home", style: {fontSize: 20, color: 'white'}}}
+      />
       <Text>Welcome {firebase.auth().currentUser.uid}</Text>
     </View>
   );
