2bf5ac8a65fe6498f1d1dbecbe2b50dbd19d3519
Henry <henrygraves95@gmail.com>
2019-10-14 17:39:14 -0700

    Cleaned up App.js

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { StyleSheet, Button } from 'react-native';
+import firebase from 'firebase';
+import { StyleSheet } from 'react-native';
 import { useScreens } from 'react-native-screens';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
+/* our imports */
 import HomeScreen from './src/screens/HomeScreen';
 import TasksScreen from './src/screens/TasksScreen';
 import AuthScreen from './src/screens/AuthScreen';
 import CreateTaskScreen from './src/screens/CreateTaskScreen';
 import CalendarScreen from './src/screens/CalendarScreen';
-useScreens();
-
-//TODO: DL - this is very inefficient to include the entire firebase SDK
-//we will need to update to only include specific SDK's once we have a better
-//idea of which ones we need
-
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import * as firebase from "firebase/app";
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
 import BillsScreen from './src/screens/BillsScreen';
 import CreateBillScreen from './src/screens/CreateBillScreen';
 
+useScreens();
 const firebaseConfig = {
   apiKey: "AIzaSyA_ZWMR-MFG_ZHNK4_WuEeHoLP9vzsY_Vk",
   authDomain: "rumate-faaeb.firebaseapp.com",
@@ -59,8 +50,6 @@ const tabNavigator = createBottomTabNavigator({
 
 const taskNavigator = createStackNavigator({
   CreateTask: CreateTaskScreen,
-}, {
-
 });
 
 const appStackNavigator = createStackNavigator({
