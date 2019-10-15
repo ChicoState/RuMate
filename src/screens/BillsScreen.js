bf683eda5895aeb25f572526e3f384fe4d00f633
Henry <henrygraves95@gmail.com>
2019-10-14 17:42:24 -0700

    removed unused imports

--- src/screens/BillsScreen.js
+++ src/screens/BillsScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import BillList from '../components/BillList';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Header } from 'react-native-elements';
 
 const renderAddBill = (navigation) => {
