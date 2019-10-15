d16692e5c0e21b21bc3ad4933ee00fa5e67a511b
Henry <henrygraves95@gmail.com>
2019-10-14 22:14:07 -0700

    add custom styling to homebaked header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Header = ({title}) => {
+const Header = ({title, color, fontSize, paddingTop, paddingBottom}) => {
   return (
     <View>
-      <Text style={styles.header}>{title}</Text>
+      <Text 
+        style={[styles.header, {
+          color, 
+          fontSize,
+          paddingTop,
+          paddingBottom
+          }]}>
+        {title}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-    paddingVertical: 50,
-    fontSize: 20,
     alignSelf: 'center'
-  }
+  },
 });
 
 export default Header;
