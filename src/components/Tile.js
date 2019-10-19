654c2447a139688d743a26e5cb874e75e5aeda4c
Henry <henrygraves95@gmail.com>
2019-10-18 19:02:17 -0700

    added styling

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,20 +4,29 @@ import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 const Tile = ({ title, color, text, textColor, location, nav }) => {
   return (
     <TouchableOpacity onPress={() => {nav.navigate(location)}}>
-      <View style={[{backgroundColor: color}, styles.wrapper]}>
-        <Text style={[styles.wrapper, {color: textColor}]}>{title}</Text>
-        <Text style={[styles.wrapper, {color: textColor}]}>{text}</Text>
+      <View style={[{backgroundColor: color, borderColor: color}, styles.wrapper]}>
+        <Text style={[styles.title, {color: textColor}]}>{title}</Text>
+        <Text style={[{color: textColor}]}>{text}</Text>
       </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    paddingBottom: '5%',
+    paddingTop: '2%'
+  },
   wrapper: {
     marginHorizontal: 5,
     marginVertical: 3,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    borderWidth: 1,
     height: 100,
-    width: "45%"
+    width: "95%",
+    alignSelf: 'center'
   }
 });
 
