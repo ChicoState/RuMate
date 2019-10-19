6367b15152f21cbe122bff0f84933d82d33da4f7
Henry <henrygraves95@gmail.com>
2019-10-18 19:02:57 -0700

    add message component

---
+++ src/components/Message.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Message = () => {
+  return (
+    <View>
+      <Text>I'm a message</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Message;
