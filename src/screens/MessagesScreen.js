92ef654f2e82df6a48ff7a42ad49bd8b476b58a2
Henry <henrygraves95@gmail.com>
2019-10-21 16:26:29 -0700

    add conversation component

--- src/screens/MessagesScreen.js
+++ src/screens/MessagesScreen.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { Header } from 'react-native-elements';
+import Conversation from '../components/Conversation';
 import Message from '../components/Message';
 
 const MessageScreen = ({ navigation }) => {
+
+  const renderConversations = () => {
+    // pull all conversations
+    return (
+      <Conversation />
+    );
+  }
+
   return (
     <View>
       <Header
         backgroundColor="orange"
-        leftComponent={{icon: 'arrow-back', onPress: () => navigation.navigate('Home')}}
-        centerComponent={{text: "Messages", style: {fontSize: 20, color: 'black'}}}
-        rightComponent={{icon: 'add-circle', onPress: () => navigation.navigate('CreateBill') }}
+        leftComponent={{
+          icon: 'arrow-back',
+          onPress: () => navigation.navigate('Home')
+        }}
+        centerComponent={{
+          text: "Messages",
+          style: { fontSize: 20, color: 'black' }
+        }}
+        rightComponent={{
+          icon: 'add-circle',
+          onPress: () => navigation.navigate('CreateMessage') 
+        }}
       />
-      <Message />
+      {renderConversations()}
     </View>
   );
 }
