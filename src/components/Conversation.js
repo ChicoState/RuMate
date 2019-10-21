80df3b9525179801ce5710e2895052f7d3d1147d
Henry <henrygraves95@gmail.com>
2019-10-21 16:26:44 -0700

    add conversation component

---
+++ src/components/Conversation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Message from '../components/Message';
+
+const Conversation = () => {
+  return (
+    <View>
+      <Message />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Conversation;
