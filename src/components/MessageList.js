a35d41b7d4e4bf71551ebc76406d60d76c9aadef
Henry <henrygraves95@gmail.com>
2019-10-22 13:11:37 -0700

    add key value and navigation props

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Conversation from './Conversation';
 
-const MessageList = () => {
+const MessageList = ({ navigation }) => {
   const renderConversations = () => {
     // pull all conversations
+    /* 
+      push conversations into a list to be rendered
+    */
     const list = [
       <Conversation
         name="RM name"
         blurb="Lorem ipsum dolor sit amet, consectetur
           adipiscing elit. Suspendisse pellentesque mollis
           lacus a euismod."
+        navigation={navigation}
+        key={1}
       />
     ];
     return list;
