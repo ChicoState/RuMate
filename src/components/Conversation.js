9f4003fa34ac628d00868140a06b964220afb9b3
Henry <henrygraves95@gmail.com>
2019-10-21 17:16:44 -0700

    removed imports added styles

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Message from '../components/Message';
 
-const Conversation = () => {
+const Conversation = ({ name, blurb }) => {
   return (
-    <View>
-      <Message />
+    <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.blurb}>{blurb}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    paddingHorizontal: 25,
+    paddingBottom: 10,
+    borderColor: '#111',
+    borderBottomWidth: 1,
+  },
+  name: {
+    fontSize: 18,
+    paddingVertical: 5,
+  },
+  blurb: {
+    fontSize: 10,
+    paddingLeft: 15,
+  }
 });
 
 export default Conversation;
