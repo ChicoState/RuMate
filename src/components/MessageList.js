c9d30daf5d19a5d3cc7f40cf14aef79152ed1a06
Henry <henrygraves95@gmail.com>
2019-10-23 06:46:50 -0700

    add realtime message rendering

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Message from '../components/Message';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, FlatList } from 'react-native-gesture-handler';
+import firebase from 'firebase';
 
-const MessageList = () => {
-  let date = new Date();
-  let time = date.toLocaleString('en-US', { 
-    hour: 'numeric', 
-    minute: 'numeric', 
-    hour12: true 
-  })
-  return (
-    <ScrollView style={styles.container}> 
-    {/*Change to flatlist */}
-      <Message
-        name="test"
-        msg="test message"
-        time={time}
-        to={false}
+const MessageList = ({ id, recipient }) => {
+  const [messages, setMessages] = useState([]);
+  const getMessages = () => {
+    setMessages([])
+    let response = firebase.database().ref('/messages');
+    let users = firebase.database().ref('/users');
+    let sender = "";
+    users.on("value", (snapshot) => {
+      let data = snapshot.val();
+      for (let item in data) {
+        if (firebase.auth().currentUser.uid == data[item].uid) {
+          sender = data[item].name;
+        }
+      }
+    })
+    let messageList = [];
+    response.on("value", (snapshot) => {
+      let data = snapshot.val();
+      for (let item in data) {
+        if ((data[item].to == recipient && data[item].from == sender) ||
+            (data[item].to == sender && data[item].from == recipient)) {
+          console.log(data[item])
+          messageList.push(data[item])
+          setMessages(messageList);
+        }
+      }
+    })
+  }
+
+  const renderMessages = () => {
+    return (
+      <FlatList style={styles.container}
+        data={messages}
+        keyExtractor={item => item.msgID}
+        inverted={0}
+        renderItem={(item) => {
+          let sentToMe = false;
+          if (item.item.senderID == firebase.auth().currentUser.uid) {
+            sentToMe = true;
+          }
+          return (
+            <Message 
+              sentToMe={sentToMe}
+              msg={item.item.text}
+              time={item.item.time}
+            />
+          )
+        }}
       />
-      <Message
-        name="test"
-        msg="test message"
-        time={time}
-        to={true}
-      />
-    </ScrollView>
-  );
+    )
+  }
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  return renderMessages();
 }
 
 const styles = StyleSheet.create({
