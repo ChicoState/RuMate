694591fec5f609c473d88ec542b3d241337b44f3
Henry <henrygraves95@gmail.com>
2019-10-22 20:26:50 -0700

    add msg styling for to and from

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Message = () => {
+const Message = ({ msg, time, to }) => {
+  to ? msgStyle = styles.to : msgStyle = styles.from;
+  to ? msgWrapper = styles.toMsgWrapper : msgWrapper = styles.fromMsgWrapper;
+
   return (
-    <View>
-      <Text>I'm a message</Text>
+    <View style={msgWrapper}>
+      <Text style={[msgStyle, styles.timeStyle]}>{time}</Text>
+      <Text style={msgStyle}>{msg}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  toMsgWrapper: {
+    marginRight: 20,
+    marginLeft: "40%",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+  },
+  fromMsgWrapper: {
+    marginLeft: 20,
+    marginRight: '40%',
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'green',
+    borderRadius: 5,
+    backgroundColor: 'green',
+    paddingVertical: 10,
+  },
+  to: {
+    color: 'white',
+    alignSelf: 'flex-end',
+    paddingHorizontal: 10,
+    fontSize: 15,
+  },
+  from: {
+    color: 'white',
+    paddingHorizontal: 10,
+    fontSize: 15,
+  },
+  timeStyle: {
+    color: 'white',
+    fontSize: 10,
+  },
+  nameStyle: {
+    fontSize: 20,
+  }
 });
 
 export default Message;
