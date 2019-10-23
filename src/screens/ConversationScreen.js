3cd7cb2a9d5be38a5662ce1a8e86fe3fcfbedfd5
Henry <henrygraves95@gmail.com>
2019-10-22 20:27:51 -0700

    add text input for message typing

--- src/screens/ConversationScreen.js
+++ src/screens/ConversationScreen.js
@@ -1,23 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, KeyboardAvoidingView, Button, TextInput } from 'react-native';
 import { Header } from 'react-native-elements';
+import MessageList from '../components/MessageList';
 
 const ConversationScreen = ({ navigation }) => {
   const name = navigation.state.params.name;
+  const [input, setInput] = useState("");
+  const [cleared, setCleared] = useState(true);
+  const submitMessage = (event) => {
+    if (event.nativeEvent.key === "Enter") {
+      setCleared(false);
+      alert("Sent")
+    }
+  }
+
   return (
-    <View>
+    <KeyboardAvoidingView
+      style={styles.keyboard}
+      behavior="padding"
+      keyboardVerticalOffset={10}
+    >
       <Header
         backgroundColor="green"
         leftComponent={{icon: 'arrow-back', onPress: () => navigation.navigate('Messages') }}
         centerComponent={{text: name, style: {fontSize: 20, color: 'black'}}}
       />
-      <Text>Conversation Screen</Text>
-    </View>
+      <MessageList />
+      <TextInput style={styles.textInput}
+          value={input}
+          onChangeText={setInput}
+          placeholder="Message"
+          multiline
+          enablesReturnKeyAutomatically={true}
+          onKeyPress={(event) => {
+            submitMessage(event);
+          }}
+          onEndEditing={() => {
+            if (!cleared) {
+              setInput("")
+            }
+          }}
+      />
+      
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-
+  textInput: {
+    borderColor: 'black',
+    borderTopWidth: 1,
+    marginBottom: "25%",
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
 });
 
 export default ConversationScreen;
