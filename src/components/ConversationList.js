a4fac3c0a2c15a3bdbbb499ca5111ad69588492d
Henry <henrygraves95@gmail.com>
2019-10-22 20:24:47 -0700

    add conditional rendering for if no conversations

---
+++ src/components/ConversationList.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Conversation from './Conversation';
+
+const ConversationList = ({ navigation }) => {
+  const [conversations, setConversations] = useState([])
+
+  const renderConversations = () => {
+    // pull all conversations
+    /* 
+      push conversations into a list to be rendered
+    */
+    if (conversations) {
+      const list = [
+        <Conversation
+          name="RM name"
+          blurb="Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Suspendisse pellentesque mollis
+            lacus a euismod."
+          navigation={navigation}
+          key={1}
+        />
+      ];
+      return list;
+    } else {
+      return <Text style={styles.noConv}>No messages</Text>
+    }
+  }
+  return (
+    <View>
+      {/* put in flatlist eventually */}
+      {renderConversations()}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  noConv: {
+    alignSelf: 'center',
+    fontSize: 30,
+    paddingTop: "30%",
+  }
+})
+
+export default ConversationList;
