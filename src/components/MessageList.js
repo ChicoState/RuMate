7ec73e2553b5aaac48e7a8df2efc152a730986da
Henry <henrygraves95@gmail.com>
2019-10-22 20:44:57 -0700

    add real time read out

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,19 +4,26 @@ import Message from '../components/Message';
 import { ScrollView } from 'react-native-gesture-handler';
 
 const MessageList = () => {
+  let date = new Date();
+  let time = date.toLocaleString('en-US', { 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    hour12: true 
+  }) // will have to use this method to store time, 
+    // for now, just setting test msgs to current time.
   return (
     <ScrollView style={styles.container}> 
     {/*Change to flatlist */}
       <Message
         name="test"
         msg="test message"
-        time="100"
+        time={time}
         to={false}
       />
       <Message
         name="test"
         msg="test message"
-        time="100"
+        time={time}
         to={true}
       />
     </ScrollView>
