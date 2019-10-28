8d11a18940d9bd3d0a7ffc366f56c16e736a79d0
David Luther <dluther@goldensoftware.com>
2019-10-28 02:48:00 -0700

    create a notifications/invitations screen

---
+++ src/screens/InvitationsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import InvitationList from '../components/InvitationList';
+
+const InvitationsScreen = () => {
+    return(
+        <View>
+            <InvitationList></InvitationList>
+        </View>
+    )
+}
+
+export default InvitationsScreen;
