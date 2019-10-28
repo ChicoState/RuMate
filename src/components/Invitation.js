91dcbabe06ca2ea2ecfd76149b19cb8b33089a9a
David Luther <dluther@goldensoftware.com>
2019-10-28 02:48:34 -0700

    beginning work on the invitations list

---
+++ src/components/Invitation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+import InvitationsScreen from '../screens/InvitationsScreen';
+
+const Invitation = () => {
+    return(
+        <View>
+            <Text>
+                Invitation
+            </Text>
+        </View>
+    )
+}
+
+export default Invitation;
---
+++ src/components/InvitationList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const InvitationList = () => {
+    return(
+        <View>
+            <Text>Invitation List</Text>
+        </View>
+    )
+}
+
+export default InvitationList;
