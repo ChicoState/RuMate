01f03089b45abd5255ec134ab730d01216612d3a
David Luther <dluther@goldensoftware.com>
2019-12-01 01:21:46 -0800

    add tasks tile to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar, ScrollView } from 'react-native';
 import { Header } from 'react-native-elements';
 import Tile from '../components/Tile';
-import firebase from 'firebase';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import * as Ani from 'react-native-animatable'
 
 const HomeScreen = ({ navigation }) => {
+  const [taskColor, setTaskColor] = useState("#111");
+  const [taskText, setTaskText] = useState("You have 0 upcoming or past-due assignments. You're a great RuMate!")
   return (
     <View style={styles.background}>
       <StatusBar barStyle='light-content'/>
@@ -18,7 +19,14 @@ const HomeScreen = ({ navigation }) => {
         rightComponent={<Icon size={30} color='white' name="group-add" onPress = {() => navigation.navigate('AddRoommate')} />}
       />
       <ScrollView style={{height: '100%'}}>
-        <Text>Welcome {firebase.auth().currentUser.uid}</Text>
+        <Tile style={styles.tile}
+          title="Tasks"
+          color={taskColor}
+          text={taskText}
+          textColor="white"
+          nav={navigation}
+          location="Tasks"
+        />
         <Tile style={styles.tile}
           title="Messages"
           color="#111"
