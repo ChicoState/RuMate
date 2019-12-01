5dd83e06fa5d688e1d100b7b6add90d98f86d591
Henry <henrygraves95@gmail.com>
2019-12-01 12:40:52 -0800

    add unit tests for Message component

---
+++ Message.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Message from './src/components/Message';
+
+describe('<Message />', () => {
+
+  it('has 1 child', () => {
+    const tree = renderer.create(<Message />).toJSON();
+    console.log(tree.children.length)
+    expect(tree.children.length).toBe(2);
+  });
+  
+  it('renders correctly', () => {
+    const tree = renderer.create(<Message />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+});
+
