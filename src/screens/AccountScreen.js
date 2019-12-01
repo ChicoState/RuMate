61c3cd66401f2082a96bc8beec2ee07721b19c19
Henry <henrygraves95@gmail.com>
2019-12-01 12:38:28 -0800

    add account options

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text, ScrollView } from 'react-native'
 import { Header } from 'react-native-elements'
+import firebase from 'firebase'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import User from 'react-native-vector-icons/FontAwesome';
+import Tile from '../components/Tile'
+
+const getDisplayName = () => {
+  let username = firebase.auth().currentUser.email.split("@")[0]
+  let capital = username[0].toUpperCase()
+  username = username.split(username[0])
+  return(capital + username[1])
+}
 
 const AccountScreen = ({ navigation }) => {
   return(
@@ -12,17 +21,52 @@ const AccountScreen = ({ navigation }) => {
         leftComponent={<Icon name='arrow-back' size={25} color='white' onPress = { () => navigation.navigate('Home')} />}
         centerComponent={{text: "My Account", style: {fontSize: 20, color: 'white'}}}
       />
-      <User style = {styles.userLogo}
-        name = "user-circle"
-        size = {100}
-      />
+      <ScrollView style={{height: '100%'}}>
+        <Text style={styles.welcomeBanner}>{getDisplayName()}</Text>
+        
+        <User style = {styles.userLogo}
+          name = "user-circle"
+          size = {100}
+        />
+        <Tile style={styles.tile}
+          title="Change Display Name"
+          color="#111"
+          text="Tap to change your display name"
+          textColor="white"
+          nav={navigation}
+          location=""
+        />
+        <Tile style={styles.tile}
+          title="Change Password"
+          color="#111"
+          text="Tap to change your password"
+          textColor="white"
+          nav={navigation}
+          location=""
+        />
+        <Tile style={styles.tile}
+          title="Delete Account"
+          color="#111"
+          text="This permanently deletes your account!"
+          textColor="red"
+          nav={navigation}
+          location=""
+        />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   userLogo: {
-    flex: 2
+    paddingTop: '10%',
+    paddingBottom: '20%',
+    alignSelf: 'center'
+  },
+  welcomeBanner: {
+    fontSize: 30,
+    alignSelf: 'center',
+    paddingTop: '20%',
   }
 });
 
