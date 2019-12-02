ba6a190a6f8223d8e24af4c5407a12fe9edc16d1
Henry <henrygraves95@gmail.com>
2019-12-02 09:05:32 -0800

    initial commit for change detail screen

---
+++ src/screens/ChangeDetailsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ChangeDetailsScreen = () => {
+  return (
+    <View>
+      <Text>I'm a change detail screen!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ChangeDetailsScreen
