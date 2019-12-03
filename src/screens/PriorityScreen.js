424260a316831e0df22592f90b0e378d930cc8cf
David Luther <dluther@goldensoftware.com>
2019-12-02 22:25:46 -0800

    display past due bills and tasks correctly

--- src/screens/PriorityScreen.js
+++ src/screens/PriorityScreen.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Header } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import firebase from 'firebase';
+import { ListItem } from 'react-native-elements';
+import BackIcon from 'react-native-vector-icons/MaterialIcons'
 
 
 const PriorityScreen = ({navigation}) => {
@@ -15,12 +17,18 @@ const PriorityScreen = ({navigation}) => {
         let data = snapshot.val();
         for (let item in data)
         {
-            if (firebase.auth().currentUser.uid == data[item].memberID)
+            console.log(data[item].uid)
+            if (firebase.auth().currentUser.uid == data[item].uid)
             {
-                the_assignments.push(data[item]);
+              let object = {
+                title: data[item].name + "- $" + data[item].value,
+                subtitle: "Due by: " + data[item].date,
+                icon: "dollar"
+              };
+              the_assignments.push(object);
             }
         }
-        setAssignments(the_assignments);
+        //setAssignments(the_assignments);
     });
 
     ref = firebase.database().ref('tasks');
@@ -28,9 +36,14 @@ const PriorityScreen = ({navigation}) => {
         let data = snapshot.val();
         for (let item in data)
         {
-            if (firebase.auth().currentUser.uid == data[item].memberID)
+            if (firebase.auth().currentUser.uid == data[item].uid)
             {
-                the_assignments.push(data[item]);
+                let object = {
+                  title: data[item].description,
+                  subtitle: "Due by: " + data[item].date,
+                  icon: "tasks"
+                }
+                the_assignments.push(object);
             }
         }
         setAssignments(the_assignments);
@@ -39,13 +52,13 @@ const PriorityScreen = ({navigation}) => {
 
   useEffect(() => {
     getAssignments();
-  })
+  }, []);
 
   return (
     <View style={{flex: 1}}>
       <Header
         backgroundColor="#000"
-        leftComponent={<Icon name='arrow-back' size={30} color='white' onPress = { () => navigation.navigate('Home')} />}
+        leftComponent={<BackIcon name='arrow-back' size={30} color='white' onPress = { () => navigation.navigate('Home')} />}
         centerComponent={{text: "Priority Assignments", style: {fontSize: 20, color: 'white'}}}
         //rightComponent={{icon: 'add-circle', color: 'white', onPress: () => navigation.navigate('CreateTask') }}
         //TODO DL: make a filter functionality to set how long upcoming should be, maybe have a past due and an upcoming section
@@ -55,8 +68,9 @@ const PriorityScreen = ({navigation}) => {
           <ListItem
             onPress={() => openDialog(l.gid)}
             key={i}
-            title={l.from}
-            rightIcon = {<Icon size={30} color='black' name="error-outline" />}
+            title={l.title}
+            subtitle={l.subtitle}
+            rightIcon = {<Icon size={30} color='black' name={l.icon} />}
             bottomDivider
           />
         ))         
