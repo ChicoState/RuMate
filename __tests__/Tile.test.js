43a40344d53143dba359f6fffebf3b48e22bcf07
Henry <henrygraves95@gmail.com>
2019-12-15 14:50:52 -0800

    add testing for logout

--- __tests__/Tile.test.js
+++ __tests__/Tile.test.js
@@ -4,16 +4,7 @@ import firebase from 'firebase';
 import { shallow, configure } from 'enzyme';
 import Tile from '../src/components/Tile';
 import Adapter from 'enzyme-adapter-react-16';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA_ZWMR-MFG_ZHNK4_WuEeHoLP9vzsY_Vk",
-  authDomain: "rumate-faaeb.firebaseapp.com",
-  databaseURL: "https://rumate-faaeb.firebaseio.com",
-  projectId: "rumate-faaeb",
-  storageBucket: "",
-  messagingSenderId: "484837108351",
-  appId: "1:484837108351:web:53a875e9546d00aa2dad4a"
-};
+import firebaseConfig from './Firebase.test.config';
 
 describe('<Tile />', () => {
 
