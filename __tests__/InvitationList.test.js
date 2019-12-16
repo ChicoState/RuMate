0ea923eda3ce817c0b787b4ab327c70d258ff92e
Henry <henrygraves95@gmail.com>
2019-12-15 16:25:33 -0800

    add testing for Invitiation List

--- __tests__/InvitationList.test.js
+++ __tests__/InvitationList.test.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, configure } from 'enzyme';
 import firebase from 'firebase'
-import App from '../App'
+import Adapter from 'enzyme-adapter-react-16';
+
 import InvitationList from '../src/components/InvitationList';
 
+configure({ adapter: new Adapter() });
+
 describe('<InvitationList />', () => {
-  it('has 1 child', () => {
-    <App />
-    if (!firebase.auth().currentUser)
-      firebase.auth().signInWithEmailAndPassword("messanger1@noyou.com", "password")
-    const tree = renderer.create(<InvitationList />).toJSON()
-    console.log(tree.children.length)
-    expect(tree.children.length).toBe(1)
-  });
-  
   it('renders correctly', () => {
     const tree = renderer.create(<InvitationList />).toJSON()
     expect(tree).toMatchSnapshot()
