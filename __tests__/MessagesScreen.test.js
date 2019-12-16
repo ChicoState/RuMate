0f532e0653f753ae4bf60a5c2d9c07db3089ed90
Henry <henrygraves95@gmail.com>
2019-12-15 16:42:25 -0800

    add nav via enzyme

--- __tests__/MessagesScreen.test.js
+++ __tests__/MessagesScreen.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../App'
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import firebase from 'firebase';
+import firebaseConfig from '../config/Firebase.config';
 import MessagesScreen from '../src/screens/MessagesScreen'
 
+
+configure({ adapter: new Adapter() });
+firebase.initializeApp(firebaseConfig);
+
 describe('<MessagesScreen />', () => {
+  
   it('has 2 child', () => {
-    <App />
-    const tree = renderer.create(<MessagesScreen />).toJSON()
+    const navigation = { navigation: jest.fn() };
+    const tree = renderer.create(
+      <MessagesScreen navigation={navigation}/>
+    ).toJSON()
     expect(tree.children.length).toBe(2)
   });
   
   it('renders correctly', () => {
-    const tree = renderer.create(<MessagesScreen />).toJSON()
+    const navigation = { navigation: jest.fn() };
+    const tree = renderer.create(
+      <MessagesScreen navigation={navigation}/>
+    ).toJSON()
     expect(tree).toMatchSnapshot()
   });
 });
